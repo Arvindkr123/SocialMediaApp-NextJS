b94fae36bc143ed418753606e6062309f16466b9
Arvind Kumar <thakurarvindkr10@gmail.com>
2024-01-06 22:07:10 +0530

    added

---
+++ client/pages/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="display-1 text-center">Home</h1>
+          <img src="images/1.jpg" alt="images" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
